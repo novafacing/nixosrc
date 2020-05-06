66b9c0cd5b5ed056093e5e6642fb50baa9428769
novafacing <rowanbhart@gmail.com>
2020-05-06 01:31:03 -0400

    Change i3 start to home manager

--- modules/desktop/i3.nix
+++ modules/desktop/i3.nix
@@ -1,12 +1,28 @@
 { config, pkgs, ... }:
 {
+  imports = [
+    <home-manager/nixos>
+  ];
   services.xserver.enable = true;
   services.xserver.autorun = false;
+  home-manager.users.novafacing.xsession = {
+    windowManager = {
+      i3 = {
+        enable = true;
+        package = pkgs.i3-gaps;
+      };
+    };
+  };
   services.xserver.layout = "us";
   services.xserver.displayManager.defaultSession = "none+i3";
   #services.xserver.desktopManager.default = "none+i3";
   services.xserver.desktopManager.xterm.enable = false;
   services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.windowManager.i3.enable = true;
-  services.xserver.libinput.enable = true;
+  services.xserver.libinput = {
+    enable = true;
+    disableWhileTyping = true;
+    naturalScrolling = true;
+    tapping = false;
+  };
+
 }
